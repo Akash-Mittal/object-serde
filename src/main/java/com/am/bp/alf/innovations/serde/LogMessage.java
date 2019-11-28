ad5e5654a2765f74f42b604a9b9344760d6871f3
Akash-Mittal <mittal.akash.adam@gmail.com>
2019-11-28 09:31:18 +0100

    Changes:1.) Removed Lpmbok annotations 2.) https://github.com/Akash-Mittal/object-serde/issues/1

--- src/main/java/com/am/bp/alf/innovations/serde/LogMessage.java
+++ src/main/java/com/am/bp/alf/innovations/serde/LogMessage.java
@@ -1,18 +1,20 @@
 package com.am.bp.alf.innovations.serde;
 
-import lombok.AllArgsConstructor;
+import java.util.Map;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class LogMessage extends Message<Log> {
 
-	private static final long serialVersionUID = 2804120722928102031L;
+    LogMessage(Map<String, String> headers, Log body) {
+        super(headers, body);
+    }
+
+    private static final long serialVersionUID = 2804120722928102031L;
 
 }
--- src/test/java/com/am/bp/alf/innovations/object_serde/SerDeTest.java
+++ src/test/java/com/am/bp/alf/innovations/object_serde/SerDeTest.java
@@ -12,25 +12,25 @@ import com.google.gson.GsonBuilder;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 public class SerDeTest {
-	Gson gson;
-	PodamFactoryImpl podamFactoryImpl;
+    private Gson gson;
+    private PodamFactoryImpl podamFactoryImpl;
 
-	@Before
-	public void setup() {
-		gson = new GsonBuilder().setPrettyPrinting().create();
-		podamFactoryImpl = new PodamFactoryImpl();
+    @Before
+    public void setup() {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        podamFactoryImpl = new PodamFactoryImpl();
 
-	}
+    }
 
-	@Test
-	public void testGivenObjectCheckIfGetConvertedToJsonAndThenDesrializeBackToObject() {
-		LogMessage logMessage = podamFactoryImpl.manufacturePojo(LogMessage.class);
-		String serJson = gson.toJson(logMessage);
-		assertNotNull(serJson);
-		System.out.println(serJson);
-		LogMessage deserLogMessageObject = gson.fromJson(serJson, LogMessage.class);
-		assertNotNull(deserLogMessageObject);
-		System.out.println(deserLogMessageObject);
-	}
+    @Test
+    public void testGivenObjectCheckIfGetConvertedToJsonAndThenDesrializeBackToObject() {
+        LogMessage logMessage = podamFactoryImpl.manufacturePojo(LogMessage.class);
+        String serJson = gson.toJson(logMessage);
+        assertNotNull(serJson);
+        System.out.println(serJson);
+        LogMessage deserLogMessageObject = gson.fromJson(serJson, LogMessage.class);
+        assertNotNull(deserLogMessageObject);
+        System.out.println(deserLogMessageObject);
+    }
 
 }
